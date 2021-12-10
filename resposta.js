30de79bdbabd1ddaecaf26e6a2d3c440f9ee6bd5
EduardoAmorim <49210157+EduardoAmorim2@users.noreply.github.com>
2021-12-10 09:48:24 -0300

    Add files via upload

---
+++ resposta.js
@@ -0,0 +1,18 @@
+function compararNumeros(a, b) {
+    return a - b;
+}
+
+function ordenar(){
+
+    var input = document.getElementById('numero');
+    var array = input.value.split(" ");
+
+    console.log(array);
+
+    if(/[A-z]/.test(input.value) || input.value == ''){
+        alert("Dados não são númericos!");
+    }
+    else{
+        document.getElementById("retorno").innerHTML = array.sort(compararNumeros);
+    }
+}
